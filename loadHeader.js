026fb015b05f8dce960688af02ed323f31b67cba
Sarah Werum <swerum@snapchat.com>
2023-05-26 11:28:32 -0700

    working version

---
+++ loadHeader.js
@@ -0,0 +1,7 @@
+src="https://code.jquery.com/jquery-3.3.1.js"
+integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+crossorigin="anonymous">
+(function(){
+    ("#header").load("header.html"); 
+    ("#footer").load("footer.html"); 
+});
