f9addd155e6912bb1a9620b2ad868cc2cb7d9315
Sarah Werum <swerum@snapchat.com>
2023-05-30 10:56:17 -0700

    works, but some issues with flex

---
+++ JS/MouseLight.js
@@ -0,0 +1,11 @@
+var light = document.getElementById('light');
+var lightSize = light.style.getPropertyValue("width");
+document.documentElement.addEventListener("mousemove", handleMouseMove);
+document.documentElement.addEventListener("mouseover", handleMouseMove);
+
+function handleMouseMove(event) {
+    // console.log(event.clientX+", "+event.clientY);
+    // console.log("Light size: "+lightSize);
+    light.style.setProperty('--light-position-y', (event.pageY - 20) + 'px');
+    light.style.setProperty('--light-position-x', (event.pageX - 20) + 'px');
+};
