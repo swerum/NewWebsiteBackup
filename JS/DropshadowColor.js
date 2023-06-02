e9dd1500b05c6932944405331bba4aeb280d8c19
swerum <sarah.elisabeth.werum@gmail.com>
2023-06-02 16:18:12 -0700

    semi-final version

--- JS/DropshadowColor.js
+++ JS/DropshadowColor.js
@@ -1,30 +1,39 @@
 
 //first color is darker, second is lighter
 var colors = {
-    yellowDropshadow :  ['#ffff00', '#ffff49'],
-    smallYellow:  ['#c2c200', '#ffff22'],
-    purpleDropshadow: ['#cc79ff', '#d38bff'],
+    yellowDropshadow :  ['#c6c600', '#ffff49'],
+    smallYellow:  ['#808000', '#ffff22'],
+    studioMagenta: ['#9000aa', '#d902ff'],
     lightPurple :['#9551bf', '#cb77ff'],
-    blueDropshadow: ['#4166ad', '#5993ff'],
+    blueDropshadow: ['#365898', '#5d95ff'],
     orangeDropshadow: ['#af5d00', '#ff873c'],
-    pinkDropshadow: ['#6a152a', '#d52c56'],
+    pinkDropshadow: ['#552c36', '#951f3c'],
     greenDropshadow: ['#58a158', '#40eb40'],
-    whiteDropshadow: ['#979797', '#cfcfcf']
+    whiteDropshadow: ['#747474', '#cfcfcf'],
+    jsYellow :  ['#9f8e10', '#ecd32f'],
+    strongerBlue: ['#4166ad', '#87b1ff'],
+    grey: ['#747474', '#8b8b8b'],
+    darkRed: ['#8e000e', '#951f3c'],
+    lightRed: ['#5d0d15'],
 }
 
 var figureImages = document.getElementsByTagName("img");
 for (var i = 0; i < figureImages.length;i++) {
     var img = figureImages[i];
+    addGlow(img);
     var grandParent = img.parentElement.parentElement;
-    if (grandParent && grandParent.tagName === "A") {
-        initImage(img);
+    if (img.parentElement.tagName === "A" || (grandParent && grandParent.tagName === "A")) {
+        addHoverGlow(img);
     }
 }
 
-function initImage(img) {
+function addGlow(img) {
     var classesString = img.parentElement.className;
     var color = getDropshadowColor(classesString);
     addDropshadow(img, color[0]);
+}
+
+function addHoverGlow(img) {
     img.addEventListener("mouseover", function(e) {
         var image = e.toElement;
         var color = getDropshadowColor(image.parentElement.className);
@@ -40,15 +49,17 @@ function initImage(img) {
 function addDropshadow(element, color, size=10) {
     var filterArgument = "drop-shadow(0px 0px "+size+"px "+color+")";
     var styleString = "filter: "+filterArgument;
-    if (element.width < 100) {
+    if (element.width < 120) {
         styleString += " "+filterArgument;
     }
     element.setAttribute("style", styleString+";");
 }
 
 function getDropshadowColor(classesString) {
-    var white = colors.whiteDropshadow;
-    if (!classesString) return white;
+    var defaultColor = [getComputedStyle(document.documentElement).getPropertyValue('--defaultDropshadow')];
+    // defaultColor = [defaultColor, defaultColor];
+    // console.log(defaultColor);
+    if (!classesString) return defaultColor;
     var classes = classesString.split(" ");
     for (var i = 0; i < classes.length; i ++) {
         var className = classes[i];
@@ -57,5 +68,32 @@ function getDropshadowColor(classesString) {
             return color;
         }
     }
-    return white;
+    return defaultColor;
+}
+
+
+//-------------- special case: steam icon -------------
+addSteamIconGlow();
+function addSteamIconGlow() {
+    var steamIcon = document.getElementById("steam-icon");
+    if (!steamIcon) return; 
+    var appIcon = getImageChild(steamIcon.parentElement);
+    if (!appIcon) return;
+    var colors = getDropshadowColor(steamIcon.parentElement.className);
+    steamIcon.addEventListener("mouseover", function(e) {
+        addDropshadow(appIcon, colors[1], 20);
+    });
+    steamIcon.addEventListener("mouseleave", function(e) {
+        addDropshadow(appIcon, colors[0]);
+    });
+
+    function getImageChild(parent) {
+        for (var i = 0; i < parent.childNodes.length; i++) {
+            // console.log(parent.childNodes[i]);
+            if (parent.childNodes[i].tagName == "IMG") {
+                return parent.childNodes[i];
+            }        
+        }
+        return null;
+    }
 }
